59086248d2a892d7190d5b704110b285fc31669f
isaac <isaac.misionero18@gmail.com>
2022-10-27 09:03:20 -0300

    commit aula 2 de cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -2,9 +2,21 @@
 
 describe('Central de Atendimento ao Cliente TAT', function() {
 
-    it('verifica o título da aplicação', function() {
+    beforeEach(function(){
         cy.visit('./src/index.html')
+    })
+    it('verifica o título da aplicação', function() {
+        
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+    it('Preencher campos e enviar o formulário', function() {
+        
+        cy.get('#firstName').type('isaac')
+        cy.get('#lastName').type('santos')
+        cy.get('#email').type('isaac.skskkss11@gmail.com')
+        cy.get('#phone').type('12 22222-2222')
+        cy.get('#open-text-area').type('sksks ksksaksksdksksksks kskskskksksksksks xkkskskskksksksdksksksksksksksksksksaksksdkskskskskskskskk sksksksksxkkskskskksksksdksksksksksk')
+        cy.get('.button').click()
     })
 
   })
